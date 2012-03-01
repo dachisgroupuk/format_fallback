48b1755b13f7cbbfaed0960d0391392d03f76f96
Alessandro Morandi <alessandro@dachisgroup.com>
2012-03-01 16:18:38 +0000

    Patched gem to add support for Rails 3.1. This might break compatibility with 3.0.

--- lib/format_fallback.rb
+++ lib/format_fallback.rb
@@ -3,17 +3,23 @@ require "format_fallback/version"
 module ActionView
   class PathSet < Array
 
-    def find_with_default_template(path, prefix = nil, partial = false, details = {}, key = nil)
+    def find_with_default_template(*args)
+      path = args[0]
+      prefix = args[1]
+      partial = args[2]
+      details = args[3] || {}
+      key = args[4]
       if prefix == "layouts"
         # Layouts have their own way of managing fallback, better leave them alone
-        find_without_default_template(path, prefix, partial, details, key)
+        find_without_default_template(*args)
       else
         begin
-          find_without_default_template(path, prefix, partial, details, key)
+          find_without_default_template(*args)
         rescue MissingTemplate => e
           raise e if details[:formats] == [:html]
-          html_details = details.dup.merge(:formats => [:html])
-          find_without_default_template(path, prefix, partial, html_details, key)
+          html_args = args.dup
+          html_args[3] = details.dup.merge(:formats => [:html])
+          find_without_default_template(*html_args)
         end
       end
     end
@@ -21,17 +27,35 @@ module ActionView
 
   end
   
+  # class Resolver
+  #   
+  #   def cached(key, prefix, name, partial)
+  #     return yield unless key && caching?
+  #     cache_content = yield
+  #     if cache_content.empty?
+  #       []
+  #     else
+  #       @cached[key][prefix][name][partial] ||= cache_content
+  #     end
+  #   end
+  #   
+  # end
   class Resolver
-    
-    def cached(key, prefix, name, partial)
-      return yield unless key && caching?
-      cache_content = yield
-      if cache_content.empty?
-        []
+    def cached_with_default_template(key, path_info, details, locals)
+      name, prefix, partial = path_info
+      locals = sort_locals(locals)
+      
+      if key && caching? && !@cached[key][name][prefix][partial][locals]
+        # We need to cache, but let's make sure we're not caching emptiness
+        result = decorate(yield, path_info, details, locals)
+        @cached[key][name][prefix][partial][locals] = result unless result.empty?
       else
-        @cached[key][prefix][name][partial] ||= cache_content
+        result = cached_without_default_template(key, path_info, details, locals) do
+          yield
+        end
       end
+      result
     end
-    
+    alias_method_chain :cached, :default_template
   end
 end
